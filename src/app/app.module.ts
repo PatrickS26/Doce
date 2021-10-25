d3428537dc9044c513b6fb2f9e9f98c5e942b9c0
Ramon Bouzon <ramonbouzon@hotmail.com>
2021-10-25 20:27:54 -0300

    Elevador semi configurado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -33,7 +34,8 @@ import { ElevadorComponent } from './elevador/elevador.component';
     FormsModule,
     HttpClientModule,
     NgbModule,
-    ModalModule.forRoot()
+    ModalModule.forRoot(),
+    BrowserAnimationsModule
   ],
   providers: [ MaquinaService,  ],
   bootstrap: [AppComponent]
--- src/app/elevador/elevador.component.ts
+++ src/app/elevador/elevador.component.ts
@@ -1,10 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition,
+  keyframes,
+  query,
+  stagger
+} from '@angular/animations';
+
+//trigger('',[
+//])
+
+//state('',style({
+//  transform: '',
+//})),
 
 @Component({
   selector: 'app-elevador',
   templateUrl: './elevador.component.html',
-  styleUrls: ['./elevador.component.css']
+  styleUrls: ['./elevador.component.css'],
+  animations: [     
+    
+    
+
+
+  ]
 })
 
 export class ElevadorComponent implements OnInit {
@@ -14,8 +37,131 @@ export class ElevadorComponent implements OnInit {
   ngOnInit(): void {
     
   }
+  currentAnda:String="T";
+  bgi:String="backgT";
+  porta:String='fecha';
+  curAndar:number=0;
+
+  changePorta(newPorta:String){
+    this.porta=newPorta;
+
+  }
+
+  andar(x: number){
+    if(x === 0){
+      
+
+      if(this.curAndar===3){
+
+        this.bgi="backgB"
+      setTimeout(()=>{
+        this.bgi="backgT"
+      },3000);
+
+        this.curAndar =0;
+
+      }else if(this.curAndar===2){
+        this.bgi="backgB"
+        setTimeout(()=>{
+          this.bgi="backgT"
+        },2000);
   
+          this.curAndar =0;
+      }else if(this.curAndar===1){
+        this.bgi="backgB"
+        setTimeout(()=>{
+          this.bgi="backgT"
+        },1000);
   
+          this.curAndar =0;
+      }
+      
+
+    }else if(x === 1){
+
+      if(this.curAndar===3){
+
+        this.bgi="backgB"
+      setTimeout(()=>{
+        this.bgi="backg"
+      },2000);
+      
+
+        this.curAndar =1;
+
+      }else if(this.curAndar===2){
+        this.bgi="backgB"
+        setTimeout(()=>{
+          this.bgi="backg"
+        },1000);
+  
+          this.curAndar =1;
+      }else if(this.curAndar===0){
+        this.bgi="backgB"
+        setTimeout(()=>{
+          this.bgi="backg"
+        },1000);
+  
+          this.curAndar =1;
+      }
+
+      
+    
+    }else if(x === 2){
+      if(this.curAndar===3){
+
+        this.bgi="backgB"
+      setTimeout(()=>{
+        this.bgi="backg2"
+      },1000);
+
+        this.curAndar =2;
+
+      }else if(this.curAndar===1){
+        this.bgi="backgB"
+        setTimeout(()=>{
+          this.bgi="backg2"
+        },1000);
+  
+          this.curAndar =2;
+      }else if(this.curAndar===0){
+        this.bgi="backgB"
+        setTimeout(()=>{
+          this.bgi="backg2"
+        },2000);
+  
+          this.curAndar =1;
+      }
+    
+    }else if(x === 3){
+      if(this.curAndar===2){
+
+        this.bgi="backgB"
+      setTimeout(()=>{
+        this.bgi="backg3"
+      },1000);
+
+        this.curAndar =3;
+
+      }else if(this.curAndar===1){
+        this.bgi="backgB"
+        setTimeout(()=>{
+          this.bgi="backg3"
+        },2000);
+  
+          this.curAndar =2;
+      }else if(this.curAndar===0){
+        this.bgi="backgB"
+        setTimeout(()=>{
+          this.bgi="backg3"
+        },3000);
+  
+          this.curAndar =3;
+      }
+
+    }
+
+  }
   
 
 }
